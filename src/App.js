ee2bb8853f48d0bf23132eb14d1181831a039024
Özgün TOPCU <dhm20009@dhmi.root>
2021-11-11 13:59:50 +0300

    some changes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
+import Expense from "./components/Expense";
+
 function App() {
   return (
     <div>
-      <h2>Let's get started! from DHMİ</h2>
+      <h2>Let's get started!</h2>
+      <p>starting react now? </p>
+      <Expense />
     </div>
   );
 }
---
+++ src/components/Expense.js
@@ -0,0 +1,5 @@
+ function Expense(){
+     return <h2> Expense Item ??? WTF</h2>
+ }
+
+ export default Expense ;
